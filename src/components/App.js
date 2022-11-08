8fce9df7fa2ac709a2b7ace86a148aae0dce0bb2
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-11-08 09:48:41 -0500

    commit tree 7b82a36ceeee1ff36ede11bbab223c812ecc0509

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
